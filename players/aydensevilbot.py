11776e08f76df85ad6f0c70d7ec68bc53376fb3e
Ayden <anewell8331@student.pps.net>
2025-11-21 17:44:41 +0000

    i have no damn clue

--- players/aydensevilbot.py
+++ players/aydensevilbot.py
@@ -1,7 +1,7 @@
 """
 Copy of random bot but I changed a lot
 """
-from typing import List, Dict, Any, Tuple
+from typing import List, Dict, Any
 import random
 
 from bot_api import PokerBotAPI, PlayerAction, GameInfoAPI
@@ -18,11 +18,34 @@ class aydenbot(PokerBotAPI):
     def __init__(self, name: str):
         super().__init__(name)
         self.hands_played = 0
+        self.play_frequency = 0.8
     
+
     def get_action(self, game_state: GameState, hole_cards: List[Card], 
                    legal_actions: List[PlayerAction], min_bet: int, max_bet: int) -> tuple:
-        """Make a random legal action"""
         
+        if game_state.round_name == "preflop":
+            return self._preflop_strategy(game_state, hole_cards, legal_actions, min_bet, max_bet)
+        else:
+            return self._postflop_strategy(game_state, hole_cards, legal_actions, min_bet, max_bet)
+
+
+
+    def _preflop_strategy(self, game_state: GameState, hole_cards: List[Card], 
+                   legal_actions: List[PlayerAction], min_bet: int, max_bet: int) -> tuple:
+
+        if random.random() > self.play_frequency:
+                if PlayerAction.CHECK in legal_actions:
+                    return PlayerAction.CHECK, 0
+                return PlayerAction.FOLD, 0
+
+    def _postflop_strategy(self, game_state: GameState, hole_cards: List[Card], 
+                   legal_actions: List[PlayerAction], min_bet: int, max_bet: int) -> tuple:
+                
+        all_cards = hole_cards + game_state.community_cards
+        hand_type, _, _ = HandEvaluator.evaluate_best_hand(all_cards)
+        hand_rank = HandEvaluator.HAND_RANKINGS[hand_type]
+
         # Choose a random legal action
         if hand_rank >= HandEvaluator.HAND_RANKINGS['two_pair']:
             if PlayerAction.RAISE in legal_actions:
@@ -40,7 +63,7 @@ class aydenbot(PokerBotAPI):
         # If raising, choose a random valid amount
         if action == PlayerAction.RAISE:
             # More realistic random raise - between min raise and pot size
-            max_raise = min(game_state.pot * 2.5, max_bet) # Raise up to 1.5x pot
+            max_raise = min(game_state.pot * 1.25, max_bet) # Raise up to 1.5x pot
             if max_raise < min_bet:
                 max_raise = min_bet
                 
@@ -50,6 +73,8 @@ class aydenbot(PokerBotAPI):
         # All other actions don't need an amount
         return action, 0
     
+
+
     def hand_complete(self, game_state: GameState, hand_result: Dict[str, Any]):
         """Track hands played"""
         self.hands_played += 1
