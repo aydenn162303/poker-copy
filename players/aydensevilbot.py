2c5ad3f05dccecf1eed5273dd47d55760b70ef02
Ayden <anewell8331@student.pps.net>
2025-11-24 23:14:15 +0000

    no more weird disqualifications

--- players/aydensevilbot.py
+++ players/aydensevilbot.py
@@ -20,6 +20,17 @@ class aydenbot(PokerBotAPI):
         super().__init__(name)
         self.hands_played = 0
         self.play_frequency = 0.8
+
+        self.premium_hands = [
+            (Rank.ACE, Rank.ACE), (Rank.KING, Rank.KING), (Rank.QUEEN, Rank.QUEEN),
+            (Rank.JACK, Rank.JACK), (Rank.TEN, Rank.TEN),
+            (Rank.ACE, Rank.KING), (Rank.ACE, Rank.QUEEN), (Rank.ACE, Rank.JACK),
+            (Rank.KING, Rank.QUEEN)
+        ]
+        self.good_suited_connectors = [
+            (Rank.KING, Rank.JACK), (Rank.QUEEN, Rank.JACK), (Rank.JACK, Rank.TEN),
+            (Rank.TEN, Rank.NINE), (Rank.NINE, Rank.EIGHT)
+        ]
     
 
     def get_action(self, game_state: GameState, hole_cards: List[Card], 
@@ -33,13 +44,39 @@ class aydenbot(PokerBotAPI):
 
 
     def _preflop_strategy(self, game_state: GameState, hole_cards: List[Card], legal_actions: List[PlayerAction], 
-                          min_bet: int, max_bet: int) -> tuple:
+                            min_bet: int, max_bet: int) -> tuple:
+            """Strategy for pre-flop betting"""
+            if len(hole_cards) != 2:
+                return PlayerAction.FOLD, 0
+            
+            card1, card2 = hole_cards
+            hand_tuple1 = (card1.rank, card2.rank)
+            hand_tuple2 = (card2.rank, card1.rank)
+            
+            is_premium = (hand_tuple1 in self.premium_hands or hand_tuple2 in self.premium_hands)
+            is_suited_connector = (card1.suit == card2.suit and 
+                                (hand_tuple1 in self.good_suited_connectors or 
+                                hand_tuple2 in self.good_suited_connectors))
+            is_pocket_pair = card1.rank == card2.rank
 
-        if random.random() > self.play_frequency:
+            if not (is_premium or is_suited_connector or is_pocket_pair):
                 if PlayerAction.CHECK in legal_actions:
                     return PlayerAction.CHECK, 0
                 return PlayerAction.FOLD, 0
-        
+                
+            # With a good hand, either raise or call
+            if PlayerAction.RAISE in legal_actions:
+                # Raise 3x the big blind
+                raise_amount = min(3 * game_state.big_blind, max_bet)
+                raise_amount = max(raise_amount, min_bet)
+                return PlayerAction.RAISE, raise_amount
+            
+            if PlayerAction.CALL in legal_actions:
+                return PlayerAction.CALL, 0
+                
+            return PlayerAction.CHECK, 0
+
+
 
     def _postflop_strategy(self, game_state: GameState, hole_cards: List[Card], 
                            legal_actions: List[PlayerAction], min_bet: int, max_bet: int) -> tuple:
@@ -51,7 +88,7 @@ class aydenbot(PokerBotAPI):
         # Choose a random legal action
         if hand_rank >= HandEvaluator.HAND_RANKINGS['two_pair']:
             if PlayerAction.RAISE in legal_actions:
-                action = PlayerAction.Raise
+                action = PlayerAction.RAISE
             elif PlayerAction.CALL in legal_actions:
                 action = PlayerAction.CALL
             elif PlayerAction.CHECK in legal_actions:
